5eee301e78cf16ab23d40caf0e619d63defaf5b0
52punk <52punk2017@gmail.com>
2021-06-22 21:31:41 +0530

    Detailed display of card if clicked using FullCard.js

--- src/components/landing_page/room_card.js
+++ src/components/landing_page/room_card.js
@@ -5,6 +5,7 @@ import FullCard from "../../pages/FullCard";
 const RoomCard = (props) => {
   const [isCardClicked, setCardClicked] = useState(false);
 
+  /*
   const changeDispHidden = () => {
     document.getElementById("room_ad_poster").classList.add("hidden");
     document.getElementById("room_buttons_conatiner").classList.add("hidden");
@@ -14,16 +15,16 @@ const RoomCard = (props) => {
     document.getElementById("room_ad_poster").classList.remove("hidden");
     document.getElementById("room_buttons_conatiner").classList.remove("hidden");
   }
-
+  */
 
   const clickHandler = () => {
     setCardClicked(!isCardClicked);
     console.log(`Clicked ${props.room.address}`);
-    isCardClicked ? changeDispTrue() : changeDispHidden() ;
+    //isCardClicked ? changeDispTrue() : changeDispHidden() ;
     
   };
 
-  return ( //isCardClicked ? <FullCard room={props.room}/> :
+  return ( isCardClicked ? <FullCard room={props.room}/> :
     <div
       id="room-card-container"
       className="room_card_container"
@@ -40,11 +41,7 @@ const RoomCard = (props) => {
         <div className="room_price_container">Rs. 5500</div>
         <div className="room_features_container">{props.room.features}</div>
         <div className="room_address_container">{props.room.address}</div>
-        <div id ="room_ad_poster" className="room_ad_poster hidden">By: EasyRooms</div>
-        <div id="room_buttons_conatiner" className="room_buttons_conatiner hidden">
-            <div className="room_favorite_button"><i class="fas fa-bookmark"></i></div>
-            <div className="room_chat_container"><i class="fab fa-facebook-messenger"></i></div>
-        </div>
+       
       </div>
     </div>
   );
